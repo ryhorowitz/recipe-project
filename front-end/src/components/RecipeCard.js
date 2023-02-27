cf50ae4d702cabc62d036fcc715f22648fa92e18
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-02-27 16:58:33 -0500

    added grid recipes cards to dom

--- front-end/src/components/RecipeCard.js
+++ front-end/src/components/RecipeCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function RecipeCard() {}
+function RecipeCard({name, img}) {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={img} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="secondary" size="sm" style={{ float: 'right' }}>Delete</Button>
+      </Card.Body>
+    </Card>
+  )
+}
 
 export default RecipeCard
--- front-end/src/components/Recipes.js
+++ front-end/src/components/Recipes.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import RecipeCard from "./RecipeCard";
 
-function Recipes() {
+function Recipes({ recipes }) {
+  const recipeCards = recipes.map((recipe) => {
+    const { id, img, name } = recipe
 
-  return(
-    <h1>RECIPES COMING HERE</h1>
+    return <Col key={id}>
+      <RecipeCard img={img} name={name} />
+      </Col>
+  })
+  return (
+    <>
+      <h1 className="mb-4">RECIPES</h1>
+      <Row md={2} xl={3} className='g-4'>
+        {recipeCards}
+
+      </Row>
+
+    </>
   )
 }
 
